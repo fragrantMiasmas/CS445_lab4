566706d589e8f4b0b51359f5b480cdb9189de398
Рувим Кондратьев <ruvim.k.1@gmail.com>
2017-09-23 09:48:57 -0700

    Change Unicycle.js to use the new drawPrimitive ()

--- public_html/Lab4/geometry/Unicycle.js
+++ public_html/Lab4/geometry/Unicycle.js
@@ -13,10 +13,8 @@ function Unicycle(){
 Unicycle.prototype.drawSeat = function(){
     stack.push();
     stack.multiply(translate(0,15.5,0));
-    stack.multiply(scalem(this.seatWidth,1,2));
-    gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
-    gl.uniform4fv(uColor, this.topColor);
-    Shapes.drawPrimitive(Shapes.cube); 
+    stack.multiply(scalem(this.seatWidth,1,2)); 
+    Shapes.drawPrimitive(Shapes.cube, { transform: stack.top (), color: this.topColor }); 
     stack.pop();
 }
 Unicycle.prototype.drawRods = function(){
@@ -24,18 +22,14 @@ Unicycle.prototype.drawRods = function(){
     stack.push();
     stack.multiply(translate(0,13.5,0));
     stack.multiply(scalem(1,3,1));
-    gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
-    gl.uniform4fv(uColor, this.topColor);
-    Shapes.drawPrimitive(Shapes.cube); 
+    Shapes.drawPrimitive(Shapes.cube, { transform: stack.top (), color: this.topColor }); 
     stack.pop();
     
     //middle rod
     stack.push();
     stack.multiply(translate(0,11.5,0));
-    stack.multiply(scalem(3,1,1));
-    gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
-    gl.uniform4fv(uColor, this.topColor);
-    Shapes.drawPrimitive(Shapes.cube); 
+    stack.multiply(scalem(3,1,1)); 
+    Shapes.drawPrimitive(Shapes.cube, { transform: stack.top (), color: this.topColor }); 
     stack.pop();
     
     
@@ -44,16 +38,12 @@ Unicycle.prototype.drawRods = function(){
     stack.multiply(translate(0,8.25,0));
     stack.push();
     stack.multiply(translate(2,0,0));
-    stack.multiply(scalem(1,7.5,1));
-    gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
-    gl.uniform4fv(uColor, this.topColor);
-    Shapes.drawPrimitive(Shapes.cube);
+    stack.multiply(scalem(1,7.5,1)); 
+    Shapes.drawPrimitive(Shapes.cube, { transform: stack.top (), color: this.topColor });
     stack.pop();
     stack.multiply(translate(-2,0,0));
-    stack.multiply(scalem(1,7.5,1));
-    gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
-    gl.uniform4fv(uColor, this.topColor);
-    Shapes.drawPrimitive(Shapes.cube);
+    stack.multiply(scalem(1,7.5,1)); 
+    Shapes.drawPrimitive(Shapes.cube, { transform: stack.top (), color: this.topColor });
     stack.pop();
 }
 Unicycle.prototype.connect = function(){
@@ -66,40 +56,32 @@ Unicycle.prototype.drawWheel = function(){
      //Wheel
     stack.push();
     stack.multiply(rotateZ(90));
-    stack.multiply(scalem(this.wheelRad,this.wheelThick,this.wheelRad));
-    gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
+    stack.multiply(scalem(this.wheelRad,this.wheelThick,this.wheelRad)); 
     console.log("Plz work!");
-    gl.uniform4fv(uColor, this.wheelColor);
-    Shapes.drawPrimitive(Shapes.cylinder);
+    Shapes.drawPrimitive(Shapes.cylinder, { transform: stack.top (), color: this.wheelColor });
     stack.pop();
 }
 Unicycle.prototype.drawAxel = function(){
      //Axle
     stack.push();
     stack.multiply(rotateZ(90));
-    stack.multiply(scalem(0.5,2,0.5));
-    gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
-    gl.uniform4fv(uColor, this.wheelColor);
-    Shapes.drawPrimitive(Shapes.cylinder);
+    stack.multiply(scalem(0.5,2,0.5)); 
+    Shapes.drawPrimitive(Shapes.cylinder, { transform: stack.top (), color: this.wheelColor });
     stack.pop();
 }
 Unicycle.prototype.addPedalSpokes = function(){
     //right pedal spoke
     stack.push();
     stack.multiply(translate(3,1,0));
-    stack.multiply(scalem(1,3,1));
-    gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
-    gl.uniform4fv(uColor, this.pedalColor);
-    Shapes.drawPrimitive(Shapes.cube);
+    stack.multiply(scalem(1,3,1)); 
+    Shapes.drawPrimitive(Shapes.cube, { transform: stack.top (), color: this.pedalColor });
     stack.pop();
 
      //left pedal spoke
     stack.push();
     stack.multiply(translate(-3,-1,0));
-    stack.multiply(scalem(1,3,1));
-    gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
-    gl.uniform4fv(uColor, this.pedalColor);
-    Shapes.drawPrimitive(Shapes.cube);
+    stack.multiply(scalem(1,3,1)); 
+    Shapes.drawPrimitive(Shapes.cube, { transform: stack.top (), color: this.pedalColor });
     stack.pop();
 }
 Unicycle.prototype.addPedals = function(){
@@ -107,27 +89,23 @@ Unicycle.prototype.addPedals = function(){
     stack.push();
     stack.multiply(translate(4,2,0));
     stack.multiply(scalem(1.5,0.75,0.75));
-    stack.multiply(rotateZ(90));
-    gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
-    gl.uniform4fv(uColor, this.pedalColor);
-    Shapes.drawPrimitive(Shapes.cylinder);
+    stack.multiply(rotateZ(90)); 
+    Shapes.drawPrimitive(Shapes.cylinder, { transform: stack.top (), color: this.pedalColor });
     stack.pop();
     
     //left pedal
 //    stack.push();
     stack.multiply(translate(-4,-2,0));
     stack.multiply(scalem(1.5,0.75,0.75));
-    stack.multiply(rotateZ(90));
-    gl.uniformMatrix4fv(uModel_view, false, flatten(stack.top()));
-    gl.uniform4fv(uColor, this.pedalColor);
-    Shapes.drawPrimitive(Shapes.cylinder);
+    stack.multiply(rotateZ(90)); 
+    Shapes.drawPrimitive(Shapes.cylinder, { transform: stack.top (), color: this.pedalColor });
     stack.pop();
 }
 Unicycle.prototype.drawUnicycle = function(){ //put it all together
     stack.push();
     this.drawSeat();
     console.log("HERe!!");
-    this.drawRods();
+    this.drawRods(); 
     this.connect();
     this.drawWheel();
     this.drawAxel();
